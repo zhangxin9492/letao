b9be1402a40ed0562ad2a256aac3d0b7eb9f9758
zhangxin9492 <zhangxin9492@gmail.com>
2018-10-09 19:27:11 +0800

    分类页面完成

--- public/myMobile/js/category.js
+++ public/myMobile/js/category.js
@@ -1,3 +1,35 @@
 /**
  * Created by zhangxin on 2018/10/9.
  */
+$(function(){
+  //获取一级分类
+  $.ajax({
+    url:'/category/queryTopCategory',
+    type:'get',
+    dataType:'json',
+    success:function(info){
+      console.log(info);
+      var str = template('category_tmp',info);
+      $('.lt_main_left ul').html(str);
+    }
+  })
+  function render(id){
+    $.ajax({
+      url:' /category/querySecondCategory',
+      type:'get',
+      dataType:'json',
+      data:{id:id||1},
+      success:function(info){
+        var str = template('category02_tmp',info);
+        $('.lt_main_right ul').html(str);
+      }
+    })
+  }
+  render();
+//  根据一级分类渲染二级分类
+    $('.lt_main_left ul').on('click','.lt_main_left ul li',function(){
+      $(this).addClass('current').siblings().removeClass('current');
+      var id = $(this).data('id');
+      render(id);
+    })
+})
---
+++ public/myMobile/js/common.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhangxin on 2018/10/9.
+ */
+//初始化区域滚动
+mui('.mui-scroll-wrapper').scroll({
+  deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+  indicators: false,//是否显示滚动条
+});
